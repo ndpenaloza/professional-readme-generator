7a5dede25822695a503086f18457876e71d24568
NDPenaloza <ndpenaloza@gmail.com>
2020-11-24 00:16:27 -0800

    added gitignore file and index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const readmeQuest = [
+    { 
+        type: "input",
+        message: "What is the title of your project?",
+        name: "projectTitle"
+    },  
+    {  
+        type: "input",
+        message: "Please write a description of your project:",
+        name: "description"
+    },
+    {  
+        type: "input",
+        message: "Please write installation instructions:",
+        name: "installation"
+    },
+    {  
+        type: "input",
+        message: "Please write usage information:",
+        name: "usage"
+    },
+    {  
+        type: "input",
+        message: "Please write in contribution guidlines:",
+        name: "contributors"
+    },
+    {  
+        type: "input",
+        message: "Please write in test instructions:",
+        name: "test"
+    },
+    {  
+        type: "",
+        message: "",
+        name: ""
+    },
+    {  
+        type: "",
+        message: "",
+        name: ""
+    },
+
+
+]
